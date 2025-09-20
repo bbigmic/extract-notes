f338ebe6d7928941507fdb9b5f4874b9fc8110c1
bbigmic <122184799+bbigmic@users.noreply.github.com>
2025-09-21 00:09:57 +0200

    deleting unwanted files

--- ssl_config.py
+++
@@ -1,9 +0,0 @@
-# Prosta konfiguracja SSL
-import ssl
-import urllib3
-
-# Wyłącz ostrzeżenia SSL
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-# Tymczasowo wyłącz weryfikację SSL dla problematycznych połączeń
-ssl._create_default_https_context = ssl._create_unverified_context
